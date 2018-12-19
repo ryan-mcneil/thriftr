98a308797b7f04ea78a23dc4ca1e7f9f128978d4
Quinn Krug <quinn.krug@gmail.com>
2018-12-18 17:16:21 -0700

    privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index'
+
+  get '/ynab', to: redirect("/ynab/auth"), as: '/ynab'
+  get "/auth/:provider/callback", to: "user#create"
+
+  get '/privacy', to: 'privacy#show'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ spec/features/user_can_login_through_ynab_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature "As a visitor" do
+  xscenario "visit login and use YNAB OAuth" do
+
+    visit root_path
+
+    expect(page).to have_button("Log in with YNAB")
+
+    click_on "Log in with YNAB"
+
+    expect(current_path).to eq(phone_path)
+
+  end
+
+  xscenario "phone number" do
+
+    visit phone_path
+
+    fill_in :phone_number, with: 6182460553
+    click_on "Submit"
+
+    expect(current_path).to eq(phone_path)
+    # I receive a text message with a 6-digit code
+    # And enter the 6-digit code into the empty code field and click “Submit”
+    fill_in :phone_code, with: 123456
+    click_on "Submit"
+
+    expect(current_path).to eq(dashboard_path)
+  end
+end
